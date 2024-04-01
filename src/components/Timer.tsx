cb0d6273e6e34a7c2695bcaf81707bb5fe9a158c
Jusu <erikvixx@gmail.com>
2024-04-01 13:48:34 -0300

    refactor: exclude useless files

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -35,15 +35,6 @@ export function Timer() {
     if (time > 0) return "Resume";
     else return "Start";
   };
-  // const hours = Math.floor(time / 60000)
-  //   .toString()
-  //   .padStart(2, "0");
-  // const minutes = Math.floor(time / 6000)
-  //   .toString()
-  //   .padStart(2, "0");
-  // const seconds = Math.floor((time / 100) % 60)
-  //   .toString()
-  //   .padStart(2, "0");
 
   useEffect(() => {
     if (started) {
